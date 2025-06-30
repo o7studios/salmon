a37f1b08b9f3d297d36fb1b8f734fbe3363b7564
Julian Siebert <mail@julian-siebert.de>
2025-06-30 18:25:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,4 @@ else
     echo "No stable build for version $MINECRAFT_VERSION found :("
 fi
 
-curl -o viaversion.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.4.1/PAPER/ViaVersion-5.4.1.jar
+curl -o ./plugins/viaversion.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.4.1/PAPER/ViaVersion-5.4.1.jar
